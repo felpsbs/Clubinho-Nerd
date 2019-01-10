2c2dea33cddd69d87677d88d4424fae8fb21bffb
Felipe Brandao <felipe.brandao97@outlook.com>
2019-01-10 16:29:25 -0200

    Começo de processo de criação de uma nova senha

--- clubinhonerd/clubinhonerd/accounts/models.py
+++ clubinhonerd/clubinhonerd/accounts/models.py
@@ -2,7 +2,7 @@ import re
 from django.db import models
 from django.core import validators
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
-
+from django.conf import settings
 
 class User(AbstractBaseUser, PermissionsMixin):
 
@@ -45,7 +45,23 @@ class User(AbstractBaseUser, PermissionsMixin):
 
 
 
+class PasswordReset(models.Model):
+
+	# Aqui é o usuário que solicitou a nova senha
+	# No django a relação 1:n, a relação fica no "n"
+	user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='Usuário', 
+		related_name='resets', on_delete=models.PROTECT
+	)
+	key = models.CharField('Chave', max_length=100, unique=True)
+	created_at = models.DateTimeField('Criado em', auto_now_add=True)
+	confirmed = models.BooleanField('Confirmado?', default=False, blank=True)
+
+	def __str__(self):
+		return '{0} em {1}'.format(self.user, self.created_at)
 
 
-
-
+	class Meta(object):
+		verbose_name = 'Nova Senha'
+		verbose_name_plural = 'Novas senhas'
+		# em ordem decrescente/ atributo da classe meta
+		ordering = ['-created_at']
