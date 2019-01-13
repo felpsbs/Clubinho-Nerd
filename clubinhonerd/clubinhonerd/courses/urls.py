ced58508d6d930f2a54dd5c962b3f80f9457b8d0
Felipe Brandao <felipe.brandao97@outlook.com>
2019-01-13 18:17:57 -0200

    Feita a parte para cancelar a inscrição em um curso

--- clubinhonerd/clubinhonerd/courses/urls.py
+++ clubinhonerd/clubinhonerd/courses/urls.py
@@ -6,7 +6,9 @@ urlpatterns = [
 	path('', views.index, name='index'),
 	re_path(r'^(?P<slug>[\w_-]+)/$', views.details, name='details'),
 	re_path(r'^(?P<slug>[\w_-]+)/inscricao/$', views.enrollment, name='enrollment'),
+	re_path(r'^(?P<slug>[\w_-]+)/cancelar-inscricao/$', views.undo_enrollment, name='undo_enrollment'),
 	re_path(r'^(?P<slug>[\w_-]+)/anuncios/$', views.announcements, name='announcements'),
+
 ]
 
 # "re" = para expressão regular
--- clubinhonerd/clubinhonerd/courses/views.py
+++ clubinhonerd/clubinhonerd/courses/views.py
@@ -48,6 +48,27 @@ def enrollment(request, slug): # fazer inscrição do user no curso
 	return redirect('dashboard')
 
 
+# Se desinscrever de um curso
+@login_required
+def undo_enrollment(request, slug):
+	# Pegando o curso
+	course = get_object_or_404(Course, slug=slug)
+	# Verificando se o user está inscrito no curso
+	enrollment = get_object_or_404(Enrollment, user=request.user, course=course)
+	
+	if request.method == 'POST':
+		enrollment.delete()
+		messages.success(request, 'Sua inscrição foi cancelada com sucesso')
+		return redirect('dashboard')
+
+	template_name = 'courses/undo_enrollment.html'
+	context = {
+		'enrollment': enrollment,
+		'course': course,
+	}
+	return render(request, template_name, context)
+
+
 @login_required
 def announcements(request, slug):
 	# Pegando o curso
@@ -56,7 +77,6 @@ def announcements(request, slug):
 	if not request.user.is_staff: 
 		# Verificando se o user está inscrito no curso
 		enrollment = get_object_or_404(Enrollment, user=request.user, course=course)
-		
 		if not enrollment.is_approved():
 			messages.error(request, 'Sua inscrição está pendente.')
 			return redirect('dashboard')
