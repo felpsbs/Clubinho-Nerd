7130dfbea58881c382febf3255eb1d64cb7d17d0
Felipe Brandao <felipe.brandao97@outlook.com>
2019-07-23 09:41:49 -0300

    Configurações do BD atualizadas

--- NerdClub/Agendamento/DEV/Agendamento/src/components/FirebaseConnection.js
+++ NerdClub/Agendamento/DEV/Agendamento/src/components/FirebaseConnection.js
@@ -2,13 +2,13 @@ import firebase from 'firebase';
 
 // Configurações do Banco
 let firebaseConfig = {
-    apiKey: "AIzaSyClGZcWnBCcHXUaU8sHFcqKrnmTizAQo8A",
-    authDomain: "agendamento-48d85.firebaseapp.com",
-    databaseURL: "https://agendamento-48d85.firebaseio.com",
-    projectId: "agendamento-48d85",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
     storageBucket: "",
-    messagingSenderId: "106060848453",
-    appId: "1:106060848453:web:255ead075786444b"
+    messagingSenderId: "",
+    appId: ""
 };
 
 // Initialize Firebase
