f7c5da8298a2d2d23782cf21c7f994c3703cf1f7
Felipe Brandao <felipe.brandao97@outlook.com>
2019-08-21 09:19:15 -0300

    Pequenas mudanças

--- NerdClub/ReservMe/DEV/backend/src/validation.js
+++ NerdClub/ReservMe/DEV/backend/src/validation.js
@@ -7,86 +7,55 @@ module.exports = {
     validateName(name) {
         // Expressão regular que só aceita letras e espaços
         const pattern = /^[a-zA-Z ]+$/;
-    
-        let result = false;
-        if(name.match(pattern)) {
-            result = true;
-        }
-    
+        let result = false; 
+        result = name.match(pattern) ? true : false;    
         return result;  
     },  
 
     validateSex(sex) {
-    
         let result = false;
-        if(sex == 'Masculino' || sex == 'Feminino') {
-            result = true; 
-        }
-    
-        return result ;
+        result = sex == 'Masculino' || sex == 'Feminino' ? true : false;
+        return result;
     },
 
     validateEmail(email) {
         const pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-        
         let result = false;
-        if(email.match(pattern)) {
-            result = true;
-        }
-    
+        result = email.match(pattern) ? true : false;    
         return result;  
     },
 
     validadeCPF(cpf) {
         const length = 14;
-    
         let result = false;
-        if(cpf.length == length) {
-            result = true;
-        }
-    
+        result = cpf.length == length ? true : false;     
         return result;
     },
     
     validadePhone(phone) {
         const length = 19;
-    
         let result = false;
-        if(phone.length == 19) {
-            result = true;
-        }
-    
+        result = phone.length == 19 ? true : false ;
         return result
     },  
 
     validatePassword(password) {
         // tem que ter no mínimo 8 e só pode conter letra e número
         const pattern = /^[0-9a-zA-Z]{8,}$/;
-
         let result = false;
-        if(password.match(pattern)) {
-            result = true;
-        }
-
+        result = password.match(pattern) ? true : false;
         return result; 
     },
 
     validatePasswordConfirmed(password, passwordConfirmed) {    
-        
         let result = false;
-        if(password == passwordConfirmed) {
-            result = true;
-        }
-    
+        result = password == passwordConfirmed ? true : false    
         return result;    
     },
 
     validadePerfil(perfil) {
         let result = true;
-        if(perfil == '') {
-            result = false;
-        }
-    
+        result = perfil == '' ? false : true ;    
         return result;
     },
 
@@ -121,6 +90,10 @@ module.exports = {
         if(!this.validatePasswordConfirmed(body.password, body.passwordConfirmed) && message == '') {
             result = false;               
             message = http.clientBadResponses['different-password'];  
+        }
+        if(!this.validadePerfil(body.perfil) && message == '') {
+            result = false;
+            message = http.clientBadResponses['invalid-perfil'];        
         }
 
         return { result: result, message: message };
